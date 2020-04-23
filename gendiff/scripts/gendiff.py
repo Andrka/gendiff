f481da943e17893fa1584e73032f15f981d1caca
Andrka <postbox4mail@gmail.com>
2020-04-23 20:19:34 +0300

    Launch project

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+""" Package is under development."""
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
