ce9ab6fa0221e949752b2ff6b795959bcc19efbd
Andrka <postbox4mail@gmail.com>
2020-10-25 12:31:10 +0300

    Improve function names

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,8 +1,3 @@
 # -*- coding:utf-8 -*-
 
 """Generate differences."""
-
-import gendiff.cli
-import gendiff.diff
-import gendiff.files
-import gendiff.values
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-"""generate differences script."""
+"""Generate differences script."""
 
 from gendiff import cli, diff
 
